bff6a5205453eb3a7b70178f983b20db0fd9591b
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2022-11-25 03:51:58 +0100

    puesto el enunciado

--- lanzador.py
+++ lanzador.py
@@ -10,6 +10,7 @@ def main():
     eleccion = solicitar_introducir_numero_extremo("Introduzca el número del ejercicio que desea ejecutar (1-3) (4 para salir)",1,4)
     print(Fore.RESET) ; os.system('cls')
     if eleccion == 1:
+        print("""Poe Dameron, líder del escuadrón negro de la Resistencia, tiene dificultades para transmitir los mensajes a la\nbase de la Resistencia, dado que los mismos son muy largos y los satélites espías de la Primera Orden los intercepta, en un\nlapso muy corto desde que se transmiten. Por lo cual, nos solicita desarrollar un algoritmo que permita comprimir los\nmensajes para enviarlos más rápido y no puedan ser interceptados. Contemplando los siguientes requerimientos, implementar\nun algoritmo que los resuelva:\n\na.crear un árbol de Huffman a partir de la siguiente tabla:\n\nb.desarrollar las funciones para comprimir y descomprimir un mensaje.\n""")
         mainej1()
         main()
     elif eleccion == 4:
