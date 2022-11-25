2f2fdf79acdb9ead5a3e67d72e0812852a26c49d
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2022-11-25 02:02:29 +0100

    añadidas clases de cola y arbol de huffman

---
+++ clases/huffman.py
@@ -0,0 +1,36 @@
+from clases.cola import Cola, arribo, atencion, cola_vacia
+class nodoArbol(object):
+    def __init__(self, info, nrr=None):
+        self.izq = None
+        self.der = None
+        self.info = info
+        self.nrr = nrr
+
+
+class nodoArbolHuffman(object):
+    def __init__(self, info, valor):
+        self.izq = None
+        self.der = None
+        self.info = info
+        self.valor = valor
+
+def insertar_nodo(raiz, dato, nrr=None):
+    if(raiz is None):
+        raiz = nodoArbol(dato, nrr)
+    else:
+        if(raiz.info > dato):
+            raiz.izq = insertar_nodo(raiz.izq, dato, nrr)
+        else:
+            raiz.der = insertar_nodo(raiz.der, dato, nrr)
+    return raiz
+
+def por_nivel(raiz):
+    cola = Cola()
+    arribo(cola, raiz)
+    while(not cola_vacia(cola)):
+        nodo = atencion(cola)
+        print(nodo.info)
+        if(nodo.izq is not None):
+            arribo(cola, nodo.izq)
+        if(nodo.der is not None):
+            arribo(cola, nodo.der)
