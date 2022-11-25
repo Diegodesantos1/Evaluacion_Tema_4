c7c546c3004ec83bc670944a59892b0921f24001
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2022-11-25 02:09:53 +0100

    añadida las funciones del arbol de huffman

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,3 +1,25 @@
-tabla_probabilidades = [['A', 0.2], ['F', 0.17], ['1', 0.13], ['3', 0.21], ['0', 0.05], ['M', 0.09], ['T', 0.15]]
+from clases.cola import Cola
+from clases.huffman import nodoArbolHuffman
+class Ejercicio1:
+    tabla_probabilidades = [['A', 0.2], ['F', 0.17], ['1', 0.13], ['3', 0.21], ['0', 0.05], ['M', 0.09], ['T', 0.15]]
 
-diccionario_convertido = {'A' : '00', '3': '01', '1' : '100', 'T': '110', 'F' : '111', '0': '1010', 'M' : '1011'}
+    diccionario_convertido = {'A' : '00', '3': '01', '1' : '100', 'T': '110', 'F' : '111', '0': '1010', 'M' : '1011'}
+
+    bosque = []
+
+    for elemento in tabla_probabilidades:
+        nodo = nodoArbolHuffman(elemento[0], elemento[1])
+        bosque.append(nodo)
+
+    for elemento in bosque:
+        print(elemento.info, elemento.valor)
+    while(len(bosque) > 1):
+        elemento1 = bosque.pop(0)
+        elemento2 = bosque.pop(0)
+        nodo = nodoArbolHuffman('', elemento1.valor+elemento2.valor)
+        nodo.izq = elemento1
+        nodo.der = elemento2
+        bosque.append(nodo)
+
+
+
